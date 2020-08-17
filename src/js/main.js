230893fe529fa296fbded370bd208406827f588b
Daniel Ronan <danielconnorronan@gmail.com>
2020-08-17 16:40:30 +0100

    created menu list and radio buttons for options to choose, created a accordion, finished aligning all content on tickets page

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@
 function hideBtn() {
     document.getElementById("more-btn").style.display = "none";
   }
+// Hides the lunch button
+function hideLunchBtn() {
+  document.getElementById("lunch-btn").style.display = "none";
+}  
